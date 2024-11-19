d7a7cd6bb16254756afb2b3799c5f18f3e9e4908
toci888 <bzapart@gmail.com>
2024-11-19 09:42:52 +0100

    xd

--- HaiaFrontWww/haia/src/services/models/models.ts
+++ HaiaFrontWww/haia/src/services/models/models.ts
@@ -31,3 +31,19 @@ interface JokeItemProps {
     post: Post;
 }
 
+interface Comment {
+    id: number;
+    jokeId: number;
+    text: string;
+}
+
+interface Joke {
+    id: number;
+    text: string;
+    comments?: Comment[]; // Pole opcjonalne
+}
+
+interface JokeProps {
+    joke: Joke;
+    userId: number; // Zakładam, że userId jest liczbą. Zmień typ, jeśli to string.
+}
